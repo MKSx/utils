94a13f3f70a99fc0573107d31f004b1b39dc5bba
Mateus <17793927+MKSx@users.noreply.github.com>
2021-10-16 23:52:55 -0300

    Update Crypt.go

--- Crypt/Crypt.go
+++ Crypt/Crypt.go
@@ -1,59 +1,122 @@
-package crypt
+package cryptoxor
 
 import (
-	"fmt"
 	"encoding/base64"
+	"errors"
 	"strings"
 )
 
-func Reverse(str string) (result string) {
-    for _, v := range str { 
-		result = string(v) + result 
-    }
-    return
+type CryptoXOR struct {
+	Internal CryptoXOR_Internal
 }
 
-func Key(key string, lenstr int) (result string){
-	keylen := len(key)
-	dif := lenstr - keylen
+type CryptoXOR_Internal struct {
+	Data   []rune
+	Key    []rune
+	KeyLen int
+}
 
-	if dif > 0 {
-		return (key + strings.Repeat(key, dif / keylen + func() int {if dif % lenstr > 0 {return 1} else {return 0}}()))[0:lenstr]
+func NewCryptoXOR(key string) (*CryptoXOR, error) {
+
+	key_len := len(key)
+
+	if key_len < 1 {
+		return nil, errors.New("Key cannot be empty")
 	}
-	return func() string {if dif < 0 {return key[0:lenstr]}else {return key}}()
+	return &CryptoXOR{
+		Internal: CryptoXOR_Internal{
+			Key:    []rune(key),
+			KeyLen: key_len,
+		},
+	}, nil
 }
 
-func XOR(str string, key string) (result string){
-	ret := []byte(str)
-	for i := range ret{
-		ret[i] = ret[i] ^ key[i]
+func (c *CryptoXOR) Encode(data string) (output string) {
+	c.Internal.Data = []rune(data)
+
+	c.Internal.Data = c.Internal.XORFunction()
+
+	output = c.Internal.B64_Encode()
+	return
+}
+
+func (c *CryptoXOR) Decode(data string) (output string, err error) {
+
+	err = c.Internal.B64_Decode(data)
+
+	if err != nil {
+		return
 	}
-	return string(ret)
+
+	output = string(c.Internal.XORFunction())
+	return
 }
 
-func Encode(str string, key string) (result string){
-	l := len(str)
+func (i *CryptoXOR_Internal) B64_Decode(data string) (err error) {
+	l := len(data)
 
-	if l < 1{
+	if l > 2 {
+		data = reverseString(data[l-2:]) + data[0:l-2]
+	}
+
+	data = reverseString(data)
+
+	dataByte, err := base64.StdEncoding.DecodeString(data)
+
+	if err != nil {
+		return
+	}
+
+	i.Data = []rune(string(dataByte))
+	return
+}
+func (i *CryptoXOR_Internal) XORFunction() (output []rune) {
+	output = make([]rune, len(i.Data))
+	for index := range i.Data {
+		output[index] = i.Data[index] ^ i.Key[index%i.KeyLen]
+	}
+	return
+}
+
+func reverseRune(data []rune) (output []rune) {
+
+	size := len(data)
+	output = make([]rune, size)
+
+	size -= 1
+	for index := range data {
+		output[index] = data[size-index]
+	}
+	return
+}
+
+func reverseString(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
+func (i *CryptoXOR_Internal) B64_Encode() (output string) {
+
+	output = reverseString(base64.StdEncoding.EncodeToString([]byte(string(i.Data))))
+
+	if len(output) > 2 {
+		output = output[2:] + reverseString(output[0:2])
+	}
+	return
+}
+
+func (x *CryptoXOR) Pad(str string) string {
+
+	return str + (func(l int) string {
+		if l != 4 {
+			return strings.Repeat("=", l)
+		}
 		return ""
-	}
-	str = XOR(str, Key(key, l))
-	return func() string {str = Reverse(base64.StdEncoding.EncodeToString([] byte(str)));if len(str) > 2{return str[2:] + Reverse(str[0:2])}else{return str}}()
+	})(4-(len(str)%4))
 }
 
-func Decode(str string, key string) (result string){
-	l := len(str)
-
-	if l < 1{
-		return ""
-	}
-	if l > 2{
-		str = Reverse(str[l - 2:]) + str[0:l - 2]
-	}
-	str = Reverse(str)
-	ret, err := base64.StdEncoding.DecodeString(str)
-	if err != nil{
-		fmt.Printf("Error decoding string: %s ", err.Error())
-	}
-	return XOR(string(ret), Key(key, len(ret)))
+func (x *CryptoXOR) Unpad(str string) string {
+	return strings.Replace(str, "=", "", 2)
 }
