9328e6633077f5e685869e3711ab64e25470b295
Mateus <17793927+MKSx@users.noreply.github.com>
2022-09-29 16:26:29 -0300

    Update linux.sh

--- ping/linux.sh
+++ ping/linux.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 for i in $(seq 1 255); do ping -t 128 -W 1 -c 1 "192.168.1.$i" | grep time=; done
+#or
+for j in $(seq 1 255); do for i in $(seq 1 255); do ping -t 128 -W 1 -c 1 "192.168.$j.$i" | grep time=; done; done
